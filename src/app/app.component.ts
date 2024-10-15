5fdb55039be82422aa8e880aa435f6cb9bb91297
Matteo <botturi.matteo@gmail.com>
2024-10-15 18:01:58 +0200

    Added the Pokemon interface
    Added a database mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit} from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  template: `<h1>Pokédex</h1>`
 })
-export class AppComponent {
-  title = 'pokedex-angular';
+export class AppComponent implements OnInit{
+  pokemonList : Pokemon[] = POKEMONS;
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[0]);
+  }
+
+  selectPokemon(pokemon : Pokemon){
+    console.log(`Vous avez cliqué sur le Pokemon ${pokemon.name}`);
+  }
 }
