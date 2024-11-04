4c44dc952451bfae1d5373c3ae72e904184f088d
Matteo <botturi.matteo@gmail.com>
2024-11-04 23:17:43 +0100

    Added border and hover directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, Directive, OnInit} from '@angular/core';
 import { POKEMONS } from './mock-pokemon-list';
 import { Pokemon } from './pokemon';
+import { NgFor, NgIf } from '@angular/common';
+import { BorderCardDirective} from './border-card.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `<h1>Pokédex</h1>`
+  imports: [NgIf, NgFor, BorderCardDirective],
+  templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit{
   pokemonList : Pokemon[] = POKEMONS;
+  pokemonSelected : Pokemon | undefined;
 
   ngOnInit() {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemon : Pokemon){
-    console.log(`Vous avez cliqué sur le Pokemon ${pokemon.name}`);
+  selectPokemon(pokemonId : string){
+    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    if(pokemon){
+      console.log(`Vous avez cliqué sur le Pokemon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Vous avez demandé un Pokemon qui n'existe pas.`);
+      this.pokemonSelected = pokemon;
+    }
   }
 }
